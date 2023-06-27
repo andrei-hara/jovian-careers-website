dc6ad89a935902d4dedee661e87bf59b76a97e8e
Harabagiu <uif67564@contiwan.com>
2023-06-27 16:37:34 +0300

    added button functionalities

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify 
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
 
@@ -15,6 +15,10 @@ def hello_jovian():
 def list_jobs():
     return jsonify(jobs)
 
+@app.route("/job/<id>")
+def show_job(id):   
+    job = load_job_from_db(id)
+    return jsonify(job)         
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=7000, debug=True)
--- database.py
+++ database.py
@@ -4,10 +4,9 @@ import os
 
 #securing the key storing it into .env
 key = dotenv_values(".env")
+engine = create_engine(key["DB_KEY"])
 
 def load_jobs_from_db():
-    engine = create_engine(key["DB_KEY"])
-
     with engine.connect() as conn:
         result = conn.execute(text("select * from jobs"))
         jobs = []
@@ -15,3 +14,15 @@ def load_jobs_from_db():
             jobs.append(row._asdict())
 
     return jobs
+
+def load_job_from_db(id):
+    with engine.connect() as conn:
+            result = conn.execute(text(f"select * from jobs where id = {id}"))
+    
+    row = result.all()
+    if len(row) == 0:
+        return None
+    else:
+        return row[0]._asdict()
+
+print(load_job_from_db(2))
