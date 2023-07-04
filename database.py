6b2fb8034426940639e53fcce315853ca737ecb1
Harabagiu <uif67564@contiwan.com>
2023-07-04 10:10:17 +0300

    modified the database connection file

--- database.py
+++ database.py
@@ -28,12 +28,8 @@ def load_job_from_db(id):
 
 def add_application_to_db(job_id, data):
      with engine.connect() as conn:
-            print(data['email'])
-            # query = text(f"INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES ({job_id}, '{data['full_name']}', '{data['email']}', '{data['linkedin']}', '{data['education']}', '{data['work_experience']}', '{data['work_experience']}')")
+            
             query = text("INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience) VALUES (:job_id, :full_name, :email, :linkedin, :education, :work_experience)")
-            # conn.execute(query, job_id=job_id, full_name=data['full_name'],
-            #              email=data['email'], linkedin=data['linkedin'],
-            #              education=data['education'], work_experience = data['work_experience'])
             bind_params = {
             'job_id': job_id,
             'full_name': data['full_name'],
@@ -42,6 +38,7 @@ def add_application_to_db(job_id, data):
             'education': data['education'],
             'work_experience': data['work_experience']
             }
+
             query = query.bindparams(**bind_params)
             conn.execute(query)
             conn.commit()
