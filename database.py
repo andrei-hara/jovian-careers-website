6f47d11399aac14ca1a9de7d4dddc9fbf99ae975
Harabagiu <uif67564@contiwan.com>
2023-07-04 10:08:21 +0300

    added application form and insert functionality for the database

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, text
 from dotenv import dotenv_values
-import os
+
 
 #securing the key storing it into .env
 key = dotenv_values(".env")
@@ -24,6 +24,27 @@ def load_job_from_db(id):
         return None
     else:
         return row[0]._asdict()
+
+
+def add_application_to_db(job_id, data):
+     with engine.connect() as conn:
+            print(data['email'])
+            # query = text(f"INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES ({job_id}, '{data['full_name']}', '{data['email']}', '{data['linkedin']}', '{data['education']}', '{data['work_experience']}', '{data['work_experience']}')")
+            query = text("INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience) VALUES (:job_id, :full_name, :email, :linkedin, :education, :work_experience)")
+            # conn.execute(query, job_id=job_id, full_name=data['full_name'],
+            #              email=data['email'], linkedin=data['linkedin'],
+            #              education=data['education'], work_experience = data['work_experience'])
+            bind_params = {
+            'job_id': job_id,
+            'full_name': data['full_name'],
+            'email': data['email'],
+            'linkedin': data['linkedin'],
+            'education': data['education'],
+            'work_experience': data['work_experience']
+            }
+            query = query.bindparams(**bind_params)
+            conn.execute(query)
+            conn.commit()
 
 
 if __name__ == "__main__":
